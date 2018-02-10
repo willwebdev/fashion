aa747a39720d4a18155a7c6fdd30da9508153689
Will Barden <5765667+willwebdev@users.noreply.github.com>
2018-02-10 21:43:59 +0000

    set up basic capsule overview on homepage (missing files)

---
+++ resources/views/home.php
@@ -0,0 +1,39 @@
+<div id="home">
+	<h1>Minimalist capsule wardrobe</h1>
+	<div id="home-intro-panel">
+		<p>Want a minimalist capsule wardrobe? You're in the right place. Check it out below.</p>
+		<p>The idea is, if you have this set of items, you can mash up a decent outfit even if you pick items at random.</p>
+		<p>Currently the capsule covers a smart-casual vibe, and more options will be added soon to extend the range.</p>
+	</div>
+	<div class="wardobe-section">
+		<h2>T-shirts</h2>
+		<img src="/images/white-tshirt.jpg" alt="White t-shirt" width="<?php echo $width; ?>" />
+		<img src="/images/grey-tshirt.jpg" alt="Grey t-shirt" width="<?php echo $width; ?>" />
+		<img src="/images/black-tshirt.jpg" alt="Black t-shirt" width="<?php echo $width; ?>" />
+	</div>
+	<div class="wardobe-section">
+		<h2>Shirts</h2>
+		<img src="/images/blue-buttondown.jpg" alt="Blue button down shirt" width="<?php echo $width; ?>" />
+		<img src="/images/white-dress.jpg" alt="White dress shirt" width="<?php echo $width; ?>" />
+	</div>
+	<div class="wardobe-section">
+		<h2>Sweaters</h2>
+		<img src="/images/navy-jumper.jpg" alt="Navy sweater" width="<?php echo $width; ?>" />
+		<img src="/images/grey-jumper.jpg" alt="Grey sweater" width="<?php echo $width; ?>" />
+		<img src="/images/black-jumper.jpg" alt="Black sweater" width="<?php echo $width; ?>" />
+	</div>
+	<div class="wardobe-section">
+		<h2>Suit jackets</h2>
+		<img src="/images/navy-jacket.jpg" alt="Navy suit jacket" width="<?php echo $width; ?>" />
+	</div>
+	<div class="wardobe-section">
+		<h2>Trousers</h2>
+		<img src="/images/grey-chinos.jpg" alt="Grey chino trousers" width="<?php echo $width; ?>" />
+		<img src="/images/blue-jeans.jpg" alt="Blue jeans" width="<?php echo $width; ?>" />
+	</div>
+	<div class="wardobe-section">
+		<h2>Shoes</h2>
+		<img src="/images/white-trainers.jpg" alt="White trainers" width="<?php echo $width; ?>" />
+		<img src="/images/brown-leather.jpg" alt="Brown leather brogue shoes" width="<?php echo $width; ?>" />
+	</div>
+</div>
